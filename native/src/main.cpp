86f9c5605589fa20f831de7f20316fa3f45edd41
Marcus10110 <markgarrison@saleae.com>
2019-11-28 10:35:19 -0800

    It works!! its total garbage, but it works!

--- native/src/main.cpp
+++ native/src/main.cpp
@@ -1,10 +1,24 @@
 #include "server.h"
 #include <iostream>
+#include <exception>
 #include "server.gen.h"
 
 int main()
 {
     std::cout << "hello world!\n";
+    Api::GraphServer server;
 
+    try
+    {
+        ParseMessage( server, R"({ 
+            "function": "DispatchAction",
+            "node_id": 7 ,
+            "action": "stuff"
+        })" );
+    }
+    catch( std::exception& ex )
+    {
+        std::cout << "error: " << ex.what() << "\n";
+    }
     return 0;
 }
--- native/src/server.cpp
+++ native/src/server.cpp
@@ -1,54 +1,68 @@
+#include <iostream>
 #include "server.h"
 
 namespace Api
 {
     int GraphServer::AddNode( std::string note_type )
     {
+        std::cout << __FUNCTION__ << " called!\n";
         return 4;
     }
 
     void GraphServer::RemoveNode( int node_id )
     {
+        std::cout << __FUNCTION__ << " called!\n";
     }
     int GraphServer::ConnectPipe( int from_node_id, std::string from_port, int from_port_index, int to_node_id, std::string to_port,
                                   int to_port_index )
     {
+        std::cout << __FUNCTION__ << " called!\n";
         return 0;
     }
     void GraphServer::DisconnectPipe( int pipe_id )
     {
+        std::cout << __FUNCTION__ << " called!\n";
     }
     int GraphServer::ConnectApi( int producer_node_id, int consumer_node_id )
     {
+        std::cout << __FUNCTION__ << " called!\n";
         return 0;
     }
     void GraphServer::DisconnectApi( int api_id )
     {
+        std::cout << __FUNCTION__ << " called!\n";
     }
     void GraphServer::PushToPipe( int node_id, std::string port, int port_id, std::string data )
 
     {
+        std::cout << __FUNCTION__ << " called!\n";
     }
     int GraphServer::SubscribeToPipe( int node_id, std::string port, int port_id )
     {
+        std::cout << __FUNCTION__ << " called!\n";
         return 0;
     }
     void GraphServer::UnsubscribeToPipe( int pipe_subscription_id )
     {
+        std::cout << __FUNCTION__ << " called!\n";
     }
     std::string GraphServer::DispatchAction( int node_id, std::string action )
     {
+        std::cout << __FUNCTION__ << " called!\n";
         return "";
     }
     std::string GraphServer::GetNodeState( int node_id )
     {
+        std::cout << __FUNCTION__ << " called!\n";
         return "";
     }
     int GraphServer::SubscribeToNodeState( int node_id )
     {
+        std::cout << __FUNCTION__ << " called!\n";
         return 0;
     }
     void GraphServer::UnsubscribeToNodeState( int state_subscription_id )
     {
+        std::cout << __FUNCTION__ << " called!\n";
     }
 } // namespace Api
